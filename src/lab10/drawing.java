c5aa97d8ec49186039b566ab626e910c52df22bc
Dasmitrich <dashmitrich@gmail.com>
2020-10-09 18:17:28 +0300

    Init

---
+++ src/lab10/drawing.java
@@ -0,0 +1,31 @@
+package lab10;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class drawing {
+    drawing() {
+        JFrame Window = new JFrame() {
+        };
+        Window.setVisible(true);
+        Window.setDefaultCloseOperation(Window.EXIT_ON_CLOSE);
+        Window.setTitle("Калькулятор");
+        Toolkit tool = Toolkit.getDefaultToolkit();
+        Dimension dim = tool.getScreenSize();
+        Window.setBounds(dim.width / 2 - 250, dim.height / 2 - 250, 300, 420);
+        Window.add(new Panel());
+    }
+}
+
+class run{
+    public static void main(String[] args) {
+        {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new drawing();
+                }
+            });
+        }
+    }
+}
