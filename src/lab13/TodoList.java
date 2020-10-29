9978414812e4afdfa238b1ef1bcfbf915aaf2a98
Dasmitrich <dashmitrich@gmail.com>
2020-10-29 12:24:12 +0300

    INIT

--- src/lab13/TodoList.java
+++ src/lab13/TodoList.java
@@ -10,7 +10,8 @@ public class TodoList {
     использовать библиотеку hashmap, map;
      */
     public ArrayList<String> catalogue = new ArrayList<>();
-    public static int i = 0;
+    int i=0;
+
 
     public void RunApp() {
         Scanner in = new Scanner(System.in);
@@ -18,13 +19,13 @@ public class TodoList {
         String temp[];
         temp = str.split(" ");
 
-
         System.out.println(temp.length);
 
         for (i = 0; i < temp.length; i++) {
             switch (temp[i]) {
                 case "ADD":
-                    if(temp[i+1].charAt(0) >= '0' && temp[i+1].charAt(0) <= '9') {
+                   // System.out.println("from add " + temp[i+1].charAt(0));
+                    if(temp[i+1].charAt(0) >= '0' && temp[i+1].charAt(0) <='9' && i+1< temp.length) {
                         Add(temp, Integer.parseInt(temp[i+1]));
                     }
                     else {
@@ -32,20 +33,21 @@ public class TodoList {
                     }
                     break;
                 case "LIST":
+                    System.out.println();
                     List();
                     break;
                 case "EDIT":
-                    Edit(temp ,Integer.parseInt(temp[i+1]), i);
+                    Edit(temp ,Integer.parseInt(temp[i+1]));
                     break;
                 case "DELETE":
                     i++;
                     Delete(Integer.parseInt(temp[i]));
-                    i++;
                     break;
                 default:
                     System.err.println("Неверный формат ввода");
             }
         }
+
     }
 
     void List() {
@@ -56,39 +58,50 @@ public class TodoList {
 
     void Delete(int num){
         catalogue.remove(num);
-        System.out.println("Дело номер " + num + "удалено.");
+        System.out.println("Дело номер: " + num + " - удалено");
     }
 
     void Add(String[] temp, int position){
-        String text = null;
+        String text = "";
 
-            i+=2;
-            if(temp[i].equals("ADD") && temp[i].equals("EDIT") && temp[i].equals("DELETE") && temp[i].equals("LIST")){
-            text = temp[i];
-            i++;
-            }
+        i++;
+            do{
+                i++;
+                //System.out.println(temp[i] + " added " + i);
+                text += temp[i] + " ";
+            } while ( i+1 < temp.length && !temp[i+1].equals("ADD") && !temp[i+1].equals("EDIT") && !temp[i+1].equals("DELETE") && !temp[i+1].equals("LIST"));
+
             catalogue.add(position, text);
-
-        System.out.println("Добавлено на позицию: " + position);
+        System.out.println("Добавлено на позицию: " + position +  " - " + text);
     }
     void Add(String[] temp){
-        String text = new String();
+        String text = "";
+        i++;
 
-        while(!temp[i].equals("ADD") && !temp[i].equals("EDIT") && !temp[i].equals("DELETE") && !temp[i].equals("LIST")){
-            System.out.println(temp[i] + " added");
-            text += temp[i];
-            System.out.println(text);
-            if(i!=temp.length)
-                i++;
-        }
+        do{
+            //System.out.println(temp[i] + " added");
+            text += temp[i] + " ";
+            i++;
+        }while (!temp[i].equals("ADD") && !temp[i].equals("EDIT") && !temp[i].equals("DELETE") && !temp[i].equals("LIST"));
+        i--;
 
         catalogue.add(text);
-        System.out.println("Добавлено в конец: " + i);
+        System.out.println("Добавлено в конец: " + text);
     }
 
-    void Edit(String[] temp, int num, int i){
-        catalogue.remove(num);
-        System.out.println("Дело номер: " + num + "заменено.");
-        Add(temp, num);
+    void Edit(String[] temp, int num){
+        String text = "";
+        i+=2;
+
+        while ( !temp[i].equals("ADD") && !temp[i].equals("EDIT") && !temp[i].equals("DELETE") && !temp[i].equals("LIST")){
+            //System.out.println(temp[i] + " added");
+            text += temp[i] + " ";
+            i++;
+        }
+
+        catalogue.set(num, text);
+        System.out.println("Дело номер: " + num + " - заменено");
+        i--;
     }
+
 }
--- src/lab13/run.java
+++ src/lab13/run.java
@@ -1,11 +1,12 @@
 package lab13;
 
-import java.util.ArrayList;
-
 public class run {
     public static void main(String[] args) {
-
+        /*
+        ПРИМЕР ВВОДА КОМАНД:
+        ADD 0 work to do ADD 1 buy something ADD 2 only fun ADD a bit more work to do ADD 3 the end DELETE 0 EDIT 1 get from restaurant LIST
+         */
         new TodoList().RunApp();
 
     }
-} // В РАЗРАБОТКЕ
+}
