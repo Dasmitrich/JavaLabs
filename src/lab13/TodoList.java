3b218a956e179a580e16482c177933d924f85eae
Dasmitrich <dashmitrich@gmail.com>
2020-10-30 18:03:10 +0300

    INIT

--- src/lab13/TodoList.java
+++ src/lab13/TodoList.java
@@ -4,11 +4,19 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class TodoList {
-    /*
-    2 задание - сделать проверку на формат электронной почты
-    использовать библиотеку hashmap, map;
-     */
+
+    public static void main(String[] args) {
+        /*
+        ПРИМЕР ВВОДА КОМАНД:
+        ADD 0 work to do ADD 1 buy something ADD 2 only fun ADD a bit more work to do ADD 3 the end DELETE 0 EDIT 1 get from restaurant LIST
+        ADD more spicy food ADD snacks ADD plasmaTV ADD 2 toys EDIT 2 I wanted to say pizza
+        ADD more spicy food ADD snacks ADD plasmaTV ADD 2 toys EDIT 2 I wanted to say pizza ADD something sweet DELETE 0 LIST
+         */
+        new TodoList().RunApp();
+    }
+
     public ArrayList<String> catalogue = new ArrayList<>();
     private int i=0;
 
